06c8d16c8385f20df9871d990b65fe9896dc254a
Yashwant <yashwant.pawar122@gmail.com>
2022-12-19 14:39:46 +0530

    got it

--- main1.tf
+++ main1.tf
@@ -1,12 +1,6 @@
 terraform {
   required_version = "1.3.6"
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "4.47.0"
-    }
-    
-  }
+  
 }
 provider "aws" {
     profile = "default"
