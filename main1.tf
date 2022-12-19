1b17c5dbab50a8bb1e0b56fed9d6fa5a7bc02c09
Yashwant <yashwant.pawar122@gmail.com>
2022-12-19 14:34:04 +0530

    got it

---
+++ main1.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_version = "1.3.6"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.47.0"
+    }
+    
+  }
+}
+provider "aws" {
+    profile = "default"
+    region = "us-east-1"
+}
+
+resource "aws_instance" "yes" {
+  ami = "ami-0b0dcb5067f052a63"
+  instance_type = "t2.Micro"
+  key_name = "LinuxKeyPair"
+  tags = {
+    "Name" = "Yashwant_New"
+    "Deparment" = "IT"
+    "costcenter" = "0000"
+    
+  }
+
+}
+
+
