db6f1b2515f3b9009c67ace951df3e0d12ac9202
Yashwant <yashwant.pawar122@gmail.com>
2022-12-19 14:41:24 +0530

    got it

--- main1.tf
+++ main1.tf
@@ -2,10 +2,6 @@ terraform {
   required_version = "1.3.6"
   
 }
-provider "aws" {
-    profile = "default"
-    region = "us-east-1"
-}
 
 resource "aws_instance" "yes" {
   ami = "ami-0b0dcb5067f052a63"
